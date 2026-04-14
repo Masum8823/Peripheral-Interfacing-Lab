79e7c9ddaaf20f6a75c78af7a6a75e1ee2bc220c
Masum8823 <masum688823@gmail.com>
2026-04-14 23:44:14 +0600

    Push Button Controlled LED Code added

---
+++ Push Button Controlled LED/push_button_controlled_led.ino
@@ -0,0 +1,16 @@
+int buttonPin = 2;
+int ledPin = 13;
+
+void setup()
+{
+  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop()
+{
+  if (digitalRead(buttonPin) == LOW)
+    digitalWrite(ledPin, HIGH);
+  else
+    digitalWrite(ledPin, LOW);
+}
